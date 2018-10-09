c9cd60bb9ad2b696ccc191cc8b760c5086ddca15
Eric Gu (@ericguuu) <eric.changning.gu@gmail.com>
2018-10-09 15:13:50 -0400

    feat: Add funtionality for specific category

--- src/web/itemcatalog/routes/main.py
+++ src/web/itemcatalog/routes/main.py
@@ -8,7 +8,8 @@ main = Blueprint('main', __name__)
 @main.route('/')
 @main.route("/home")
 def index():
+    """returns all items and categories"""
     categories = Category.query.filter(Category.item_total.isnot(None)).order_by(Category.name.asc()) # noqa:501
     items = Item.query.order_by(Item.update_date.desc())
-    return render_template('home.html', title='Home', categories=categories,
+    return render_template('main.html', title='Home', categories=categories,
                            items=items)
